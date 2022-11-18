031393b56e35c2043d809c6f37fa92013748491a
jesus <jesus_lopez1997@hotmail.com>
2022-11-17 23:59:26 -0400

    separate supabase env variable

--- db/supabase.js
+++ db/supabase.js
@@ -2,7 +2,9 @@ const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
 const supabaseUrl = "https://hjipviyruaqegflufrht.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey1 = process.env.SUPABASE_KEY_1;
+const supabaseKey2 = process.env.SUPABASE_KEY_2;
+const supabaseKey = supabaseKey1 + supabaseKey2;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
